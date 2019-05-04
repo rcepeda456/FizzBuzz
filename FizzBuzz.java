feb5227f78dc282eb16daab736ea4ea787088e78
Raymond Cepeda <33160305+rcepeda456@users.noreply.github.com>
2019-05-03 21:23:31 -0400

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,45 @@
+package pratice;
+public class FizzBuzz 
+{ 
+    public static void main(String args[]) 
+    {  
+    int	x=100;
+    
+ for(int i=1; i<=x; i++) {
+	 if(i%15==0) System.out.println("Raymond OG");
+	 else if(i%3==0) System.out.println("Ray");
+	 else if(i%5==0) System.out.println("OG1");
+	 else System.out.println(i);
+ }
+    }
+}
+//
+//class FizzBuzz 
+//{ 
+//    public static void main(String args[]) 
+//    {  
+//        int n = 100; 
+//  
+//        // loop for 100 times 
+//        for (int i=1; i<=n; i++)                                  
+//        { 
+//            if (i%15==0)                                                  
+//                System.out.print("FizzBuzz"+" ");  
+//            // number divisible by 5, print 'Buzz'  
+//            // in place of the number 
+//            else if (i%5==0)      
+//                System.out.print("Buzz"+" ");  
+//  
+//            // number divisible by 3, print 'Fizz'  
+//            // in place of the number 
+//            else if (i%3==0)      
+//                System.out.print("Fizz"+" ");  
+//  
+//            // number divisible by 15(divisible by 
+//            // both 3 & 5), print 'FizzBuzz' in  
+//            // place of the number 
+//                  
+//            else // print the numbers 
+//                System.out.print(i+" ");                          
+//        } 
+//    }
